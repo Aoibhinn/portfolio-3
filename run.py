e49a06889474367724d388006b35ca35d2c5a1bf
Aoibhinn <97aoibhinn@gmail.com>
2021-07-24 16:27:06 +0000

    Add validation to check that exactly 8 values are provided by the user

--- run.py
+++ run.py
@@ -13,12 +13,28 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Portfolio 3 - Covid Stats')
 
 def get_survey_data():
+
+    """
+    Get the most recent survey data from user
+    """
+
     print ('Please enter survey data from the most recent respondent')
     print ('Data must be a string value and seperated by  commas')
     print ('Example: 1, Employed, 21 - 24, Female, Yes, Yes, Yes, No\n')
 
     data_str = input('Enter your data here: ')
-    print(f"The data provided is {data_str}")
+    
+    survey_data = data_str.split(",")
+    validate_data(survey_data)
+
+def validate_data(values):
+    try:
+        if len(values) != 8:
+            raise ValueError(
+                f"Exactly 8 values are required, you provided {len(values)}"
+            )
+    except ValueError as e: 
+        print(f"Invalid data: {e}, please try again.")
 
 
 get_survey_data()
